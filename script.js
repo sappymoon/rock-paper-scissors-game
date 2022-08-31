8374d124e8b18a8190ce76c5a2d516bf78662f4f
sappymoon <vietjohnnydiep@gmail.com>
2022-08-31 15:55:21 -0500

    made function to return random choice

---
+++ script.js
@@ -0,0 +1,19 @@
+function computerSelection(){
+    let choices = ['rock', 'paper', 'scissors']
+    let computerSelection = choices[Math.floor(Math.random() * choices.length)];
+    return computerSelection;
+}
+
+function playerSelection(playerChoice){
+
+}
+
+function playRound(playerSelection, computerSelection){
+
+}
+
+function game(){
+
+}
+
+console.log(computerSelection());
